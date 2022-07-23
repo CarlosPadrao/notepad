b2f9fa45879fc313d5f9bcafaa87550edff003a8
carlos_padrao <carlospadrao@baroes.com.br>
2022-07-23 03:30:28 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Tarefas</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="boxApp">
+
+        <div class="edit-note">
+            <i class="fa-solid fa-arrow-left"></i>
+            <textarea class="note-title-input" placeholder="Title"></textarea>
+            <textarea class="note-body-input" placeholder="Start typing"></textarea>
+        </div>
+
+        <div class="note-box">
+        </div>
+
+        <button class="add-note-btn"><i class="fa-solid fa-plus"></i></button>
+        
+    </div>
+    <script src="javascript/javascript.js"></script>
+</body>
+</html>
